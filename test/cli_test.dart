c84753d9032a860974b97ea6073feb93a02d6339
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-05-17 16:36:03 +1000

    Remove unnecessary comments & Update CLI tests
    Remove the unnecessary comments from `CLI` process unit tests from the
    previous workings out and add additional tests to demonstrate it can read
    other files, as well as all the requried Toy Robot specification tests.

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -6,64 +6,33 @@ import 'package:test/test.dart';
 
 import 'package:toy_robot_dart/toy_robot.dart';
 
-// List<String> log = [];
-// var log = [];
-// void print(String s) => log.add(s);
-// var printLog = [];
-// void print(String s) => printLog.add(s);
-
 void main() {
-  // setUp(() {
-  //   log = [];
-  // });
-
-  // tearDown(() {
-  //   log = null;
-  // });
-
   group("CLI run", () {
-    // setUp(() {
-    //   log = [];
-    // });
-
-    // tearDown(() {
-    //   log = null;
-    // });
-
     test("can process Example A", () {
       Process.start('bin/app', ["examples/example_a.txt"]).then((Process process) {
         process.stdout
           .transform(UTF8.decoder)
-          .listen((data) { expect(data, ["0,0,NORTH"]); });
-        process.stdin.writeln('Hello, world!');
-        process.stdin.writeln('Hello, galaxy!');
-        process.stdin.writeln('Hello, universe!');
+          .listen((data) { expect(data, "0,0,NORTH\n"); });
       });
-      // CLI.run([]);
-      // expect(log, ["0,0,NORTH"]);
     });
-  // });
 
-  // group("CLI proccesInput", () {
-    // setUp(() {
-    //   log = [];
-    // });
+    test("can process Example B", () {
+      Process.start('bin/app', ["examples/example_b.txt"]).then((Process process) {
+        process.stdout
+          .transform(UTF8.decoder)
+          .listen((data) { expect(data, "0,0,WEST\n"); });
+      });
+    });
 
-    // tearDown(() {
-    //   log = null;
-    // });
+    test("can process Example C", () {
+      Process.start('bin/app', ["examples/example_c.txt"]).then((Process process) {
+        process.stdout
+          .transform(UTF8.decoder)
+          .listen((data) { expect(data, "3,3,NORTH\n"); });
+      });
+    });
 
-    // test("can process from an input stream", () {
     test("can REPORT from standard input stream", () {
-      // Robot robot = new Robot();
-      // final file = new File("examples/example_a.txt");
-      // Stream<List<int>> inputStream = file.openRead();
-
-      // var log = [];
-      // void print(String s) => log.add(s);
-      // CLI.processInput(inputStream, robot);
-      // expect(CLI.processInput(inputStream, robot), ["0,0,NORTH"]);
-
       Process.start('bin/app', []).then((Process process) {
         process.stdout
           .transform(UTF8.decoder)
@@ -74,13 +43,6 @@ void main() {
     });
 
     test("can EXIT from standard input stream", () {
-      // Robot robot = new Robot();
-      // Stream input = new Stream.fromIterable([UTF8.encode("EXIT\n")]);
-
-      // // CLI.processInput(input, robot);
-      // // var completer = new Completer<String>();
-      // expect(CLI.processInput(input, robot), []);
-
       Process.start('bin/app', []).then((Process process) {
         process.stdout
           .transform(UTF8.decoder)
@@ -89,29 +51,30 @@ void main() {
       });
     });
 
-    // test("can REPORT from standard input stream", () {
-    //   Robot robot = new Robot();
-    //   Stream input = new Stream.fromIterable([UTF8.encode("REPORT\nEXIT\n")]);
-
-    //   // CLI.processInput(input, robot);
-    //   // print(log.toString());
-    //   // await expect(input, emits("0,0,NORTH"));
-    //   // expect(log, ["0,0,NORTH"]);
-    //   expect(CLI.processInput(input, robot), ["0,0,NORTH"]);
-    // });
+    test("can process all commands from standard input stream", () {
+      Process.start('bin/app', []).then((Process process) {
+        process.stdout
+          .transform(UTF8.decoder)
+          .listen((data) {
+            expect(data, [
+              "0,0,NORTH",
+              "0,1,EAST",
+              "0,2,WEST",
+              "3,3,SOUTH"
+            ]);
+          });
+        process.stdin.writeln("REPORT");
+        process.stdin.writeln("MOVE");
+        process.stdin.writeln("RIGHT");
+        process.stdin.writeln("REPORT");
+        process.stdin.writeln("MOVE");
+        process.stdin.writeln("LEFT");
+        process.stdin.writeln("LEFT");
+        process.stdin.writeln("REPORT");
+        process.stdin.writeln("PLACE 3,3,SOUTH");
+        process.stdin.writeln("REPORT");
+        process.stdin.writeln("EXIT");
+      });
+    });
   });
 }
-
-// overridePrint(testFn()) => () {
-//   var spec = new ZoneSpecification(
-//     print: (_, __, ___, String msg) {
-//       // if (log == null) { log = []; }
-//       // Add to log instead of printing to stdout
-//       log.add(msg);
-//     }
-//   );
-//   return Zone.current.fork(specification: spec).run(testFn);
-// };
-
-// Iterator<String> getStringIterator(String s) =>
-//   s.runes.map((r) => new String.fromCharCode(r)).iterator;
