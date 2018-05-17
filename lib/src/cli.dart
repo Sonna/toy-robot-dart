d3e38dc73929a9e18e0643be5a3d5af3f1ea0a3d
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-05-17 12:31:01 +1000

    Update Toy Robot CLI class methods to accept input
    Update the Toy Robot CLI static class methods to loop over the contents
    of a File or Standard Input `stdin` stream, line by line, in order to
    pass commands to a robot instance that will then process that input and
    execute the appropriate methods on itself (in accordance to the Toy
    Robot Specification).
    
    == References:
    - [Asynchronous Programming: Streams \| Dart]
      (https://www.dartlang.org/tutorials/language/streams#single-subscription-streams)
    
    - [dart \- Cancel stream onData \- Stack Overflow]
      (https://stackoverflow.com/questions/44450758/cancel-stream-ondata)
    
    - [An Introduction to the dart:io Library \| Dart]
      (https://www.dartlang.org/articles/dart-vm/io#file-system-access)

--- lib/src/cli.dart
+++ lib/src/cli.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 
 // import 'package:toy_robot_dart/src/robot.dart';
 // import '../src/robot.dart';
@@ -6,12 +8,39 @@ import 'robot.dart';
 
 class CLI {
   static void run(List<String> arguments) {
+    Stream<List<int>> inputStream = stdin;
     Robot robot = new Robot();
 
-    processInput(stdin, robot);
+    if (arguments.length > 0) {
+      final file = new File(arguments[0]);
+      inputStream = file.openRead();
+    }
+
+    processInput(inputStream, robot);
   }
 
   static void processInput(Stream source, Robot robot) {
-    robot.report();
+    // Needs to be declared ahead of `listen` for `cancel()` to be callable
+    StreamSubscription<String> subscription;
+    subscription = source
+      .transform(UTF8.decoder)       // Decode bytes to UTF8.
+      .transform(new LineSplitter()) // Convert stream to individual lines.
+      .listen((String line) {        // Process results.
+          List<String> commands = line.split(" ");
+          if (line == "EXIT") {
+            subscription.cancel();
+          }
+
+          if (commands.length > 0) {
+            if (commands.length > 1) {
+
+              robot.exec(commands[0], commands[1]);
+            } else {
+              robot.exec(commands[0]);
+            }
+          }
+        },
+        onDone: () { /* End of File Stream */ },
+        onError: (e) { print(e.toString()); });
   }
 }
